853d6732294975b8753e93fa56f08983c08184ef
yudin yaroslav <yarikyudin@mail.ru>
2021-08-21 18:37:53 +0300

    add from-add-box

--- product_list/src/App.js
+++ product_list/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddBox from './components/AddBox';
 import Header from './components/Headers';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddBox />
     </div>
   );
 }
---
+++ product_list/src/components/AddBox.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./AddBox.css"
+
+function AddBox () {
+    return (
+        <div className="add-div">
+            <form className="add-form">
+                <label className="name-label">
+                    <span className="text-label">
+                        Наименование товара <span className="required-dot">•</span>   
+                    </span>
+                    <input placeholder="Введите наименование товара" type="text" required/>
+                </label>
+                <label className="description-label">
+                    <span className="text-label">Описание товара</span>
+                    <textarea  rows="5" cols="30" placeholder="Введите описание товара"></textarea>
+                </label>
+                <label className="url-label">
+                    <span className="text-label">
+                        Ссылка на изображение товара <span className="required-dot">•</span>   
+                    </span>
+                    <input type="text" placeholder="Введите ссылку" required/>
+                </label>
+                <label className="price-label">
+                    <span className="text-label">
+                        Цена товара <span className="required-dot">•</span>   
+                    </span>
+                    <input type="text" placeholder="Введите цену" required/>
+                </label>
+                <button className="add-button">Добавить товар</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddBox;
