460a7d3521d1b028cbc6deeb5d2a37c62e20efab
yudin yaroslav <yarikyudin@mail.ru>
2021-08-23 14:52:18 +0300

    chenge css

--- product_list/src/components/Headers.js
+++ product_list/src/components/Headers.js
@@ -7,11 +7,10 @@ function Header ({sortProducts, products}) {
         <div className="header">
             <h1 className="header-text">Добавление товара</h1>
             <select id="select" className="header-button" onClick={() => {sortProducts(products, document.getElementById("select").value)}}>
-                <option className="button-text" value="По умолчанию">По умолчанию</option>
-                <option className="button-text" value="По возрастанию цены">По возрастанию цены</option>
-                <option className="button-text" value="По убыванию цены">По убыванию цены</option>
-                <option className="button-text" value="По алфавиту">По алфавиту</option>
-                <img src="/images/row.svg" alt="row-down" className="button-row"/>
+                <option value="По умолчанию">По умолчанию</option>
+                <option value="По возрастанию цены">По возрастанию цены</option>
+                <option value="По убыванию цены">По убыванию цены</option>
+                <option value="По алфавиту">По алфавиту</option>
             </select>
         </div>
     );
